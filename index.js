db97a3f0a4438ab5e776967cf2e8a0eaf4c3b7b7
zwttt <zwt@zhuwentaodeMacBook-Air.local>
2025-02-26 14:36:26 +0300

    init

--- index.js
+++ index.js
@@ -11,31 +11,10 @@ document.addEventListener('DOMContentLoaded', function () {
                     <img src="${product.image}" alt="${product.name}">
                     <h3>${product.name}</h3>
                     <p>${product.description}</p>
-                    <div class="product-footer">
-                        <p><strong>$${product.price}</strong></p>
-                        <div class="quantity-selector">
-                            <button onclick="updateQuantity(${product.id}, 'decrease')">-</button>
-                            <input type="number" id="quantity-${product.id}" value="1" min="1">
-                            <button onclick="updateQuantity(${product.id}, 'increase')">+</button>
-                        </div>
-                    </div>
+                    <p><strong>$${product.price}</strong></p>
                 `;
                 productList.appendChild(productItem);
             });
         })
         .catch(error => console.error('Error loading the product data:', error));
 });
-
-// 更新商品数量
-function updateQuantity(productId, action) {
-    const quantityInput = document.getElementById(`quantity-${productId}`);
-    let currentQuantity = parseInt(quantityInput.value);
-    
-    if (action === 'increase') {
-        currentQuantity++;
-    } else if (action === 'decrease' && currentQuantity > 1) {
-        currentQuantity--;
-    }
-
-    quantityInput.value = currentQuantity;
-}
